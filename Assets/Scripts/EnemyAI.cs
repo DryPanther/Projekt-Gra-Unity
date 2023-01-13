0d9bf69eb9baaa0d7a02c73c00c2683e10522689
Manuel Hływa <77339484+DryPanther@users.noreply.github.com>
2023-01-14 00:39:20 +0100

    Revert "Enemy działa?"
    This reverts commit a324967e3556d43f2cdc94ef166b4b1244396e33.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public GameObject Player;
-    public float startingSpeed;
-    private float speed;
+    public float speed;
     public float distance;
     public Animator animate;
-    private bool facingRight = true;
-    public float vision = 8;
+
     void Start()
     {
-        startingSpeed = 1.5f;
+        speed = 1.5f;
     }
 
     // Update is called once per frame
@@ -24,37 +22,16 @@ public class EnemyAI : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        if (direction.x > 0 && !facingRight)
-        {
-            Flip();
-        }
-        if (direction.x < 0 && facingRight)
-        {
-            Flip();
-        }
+        
 
-        if(distance < vision)
+        if (distance < 8)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed * Time.deltaTime);
-            speed = startingSpeed;
             animate.SetFloat("Speed", 1);
         }
-        if (animate.GetFloat("Attack1") == 1 || animate.GetFloat("Die1") == 1 || distance > vision)
+        if (distance > 8)
         {
-            animate.SetFloat("Speed", 0);
-            speed = 0;
+            animate.SetFloat("Speed",0);
         }
-
-
-
-    }
-    void Flip()
-    {
-        facingRight = !facingRight;
-
-        Vector2 currentScale = transform.localScale;
-        currentScale.x *= -1;
-
-        transform.localScale = currentScale;
     }
 }
