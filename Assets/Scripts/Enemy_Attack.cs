8304f00e3b49ee1c4c83ae0ad801da83c6ff87f3
Manuel Hływa <77339484+DryPanther@users.noreply.github.com>
2023-01-26 23:48:23 +0100

    Niebieski Slime

--- Assets/Scripts/Enemy_Attack.cs
+++ Assets/Scripts/Enemy_Attack.cs
@@ -6,18 +6,18 @@ public class Enemy_Attack : Enemy
 {
     public float nextAttackTime = 0f;
     public float attackRate = 0.5f;
+    public int damage;
+    void start()
+    {
+        damage = 20;
+    }
     void Update()
     {
 
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-            animate.SetTrigger("Attack");
-            animate.SetFloat("Attack1", 1);
-            Invoke("Go", 0.7f);
-        }
+        damage = 20;
     }
     private void Go()
     {
@@ -26,21 +26,31 @@ public class Enemy_Attack : Enemy
     private void OnTriggerStay2D(Collider2D other)
     {
 
-        if (Time.time >= nextAttackTime)
+        if (other.gameObject.tag == "Player")
         {
-            if (other.gameObject.tag == "Player")
+            if (Time.time >= nextAttackTime)
             {
-                nextAttackTime = Time.time + 1f / attackRate;
+                animate.SetTrigger("Attack");
+                animate.SetFloat("Attack1", 1);
+                Invoke("Go", 0.7f);
+                if (other.gameObject.tag == "Player")
+                {
+                    nextAttackTime = Time.time + 1f / attackRate;
 
-                StartCoroutine(Swing(other));
+                    StartCoroutine(Swing(other));
 
+                }
             }
         }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        damage = 0;
     }
     IEnumerator Swing(Collider2D other)
     {
         yield return new WaitForSeconds(0.6f);
-        other.GetComponent<PlayerCombat>().GetHit(20);
+        other.GetComponent<PlayerCombat>().GetHit(damage);
     }
 
 }
